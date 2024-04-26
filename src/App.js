fcd7b273f4fdebc19bbab0feb69d13f251077c42
K Anirudh <anirudh.k@beehyv.com>
2024-04-26 18:08:44 +0530

    view recent block

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import { JsonView } from "react-json-view-lite";
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +11,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,16 +21,31 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockData, setBlockData] = useState();
 
   useEffect(() => {
+    async function getBlockData(blockNumber) {
+      const blockData = await alchemy.core.getBlockWithTransactions(
+        blockNumber
+      );
+      setBlockData(blockData);
+    }
+
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      const blockNo = await alchemy.core.getBlockNumber();
+      setBlockNumber(blockNo);
+      getBlockData(blockNo);
     }
 
     getBlockNumber();
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (
+    <div className="App">
+      <h1 className="block-title">Latest Block: {blockNumber}</h1>
+      <JsonView data={blockData} style={{ basicChildStyle: "object-value" }} />
+    </div>
+  );
 }
 
 export default App;
